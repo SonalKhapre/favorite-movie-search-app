6ee8ddd261413a78b338de11be327bb86eadd50f
SonalKhapre <137900422+SonalKhapre@users.noreply.github.com>
2023-10-10 14:32:27 +0530

    Initial commit

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -7,9 +7,9 @@ const Movielist = (props) => {
   return (
     <>
     <div className='movie-container' >
-        {props.movies.map((movie, index)=>(
+        {props.movies.map((movie)=>(
             <div className='div image-container'>
-                <img src={movie.Poster} alt='movie' height={350} width={250}></img>
+                <img  key={movie.id} src={movie.Poster} alt='movie' height={350} width={250}></img>
                 <div className='overlay' onClick={ () => props.handleFavouriteClicks(movie)}>
                 <FavouriteComponent/>
                 </div>
